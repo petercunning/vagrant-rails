5bc62acd94d6e9bf382d4475216f4c121f1636fe
petercunning <peter.cunning@gmail.com>
2016-01-14 14:51:34 +0000

    update

--- script/vagrant_bootstrap.sh
+++ script/vagrant_bootstrap.sh
@@ -9,20 +9,26 @@ echo 'gem: --no-rdoc --no-ri' >> ~/.gemrc
 
 # essentials
 sudo apt-get -y update
-sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev libcurl4-openssl-dev curl wget libmysqlclient-dev phantomjs zsh sudo apt-get install qt4-dev-tools
+sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev libcurl4-openssl-dev curl wget
 
 
-# SQLite, Git and Node.js
-sudo apt-get install -y libsqlite3-dev git nodejs
+# SQLite, Git, Node.js & Phantom.js
+sudo apt-get install -y libsqlite3-dev git nodejs phantomjs
+
+# mysql
+sudo apt-get install -y libmysqlclient-dev 
 
 # Postgres
 sudo apt-get install -y postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3
 
-#setup Heroku toolbelt
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+#setup Zsh 
+sudo apt-get install -y zsh qt4-dev-tools
 
 #setup oh-my-zsh
 sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# setup Heroku toolbelt
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # setup rbenv and ruby-build
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
@@ -37,7 +43,6 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 export RBENV_ROOT="${HOME}/.rbenv"
 export PATH="${RBENV_ROOT}/bin:${PATH}"
 export PATH="${RBENV_ROOT}/shims:${PATH}"
-#rbenv install 2.1.4
 rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv install 2.1.7
